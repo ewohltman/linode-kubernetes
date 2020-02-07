89869faeeda6e937fda1f4b773801a709084adc7
Eric Wohltman <eric.wohltman@gmail.com>
2020-02-07 07:51:25 -0500

    Change directory names to match corresponding namespace names

--- deploy.sh
+++ deploy.sh
@@ -7,23 +7,23 @@ SCRIPT_PATH=$(readlink -f "${0}")
 SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
 
 EPHEMERAL_ROLES_DIR="${SCRIPT_DIR}/ephemeral-roles"
-CONTOUR_DIR="${SCRIPT_DIR}/contour"
-EFK_DIR="${SCRIPT_DIR}/efk"
-KUBE_PROMETHEUS_DIR="${SCRIPT_DIR}/kube-prometheus"
+PROJECTCONTOUR_DIR="${SCRIPT_DIR}/projectcontour"
+LOGGING_DIR="${SCRIPT_DIR}/logging"
+MONITORING_DIR="${SCRIPT_DIR}/monitoring"
 
 setup() {
   pushd . > /dev/null 2>&1 && cd "${SCRIPT_DIR}"
 }
 
 build() {
-  "${KUBE_PROMETHEUS_DIR}/build.sh"
+  "${MONITORING_DIR}/build.sh"
 }
 
 deploy() {
   "${EPHEMERAL_ROLES_DIR}/deploy.sh"
-  "${CONTOUR_DIR}/deploy.sh"
-  "${EFK_DIR}/deploy.sh"
-  "${KUBE_PROMETHEUS_DIR}/deploy.sh"
+  "${PROJECTCONTOUR_DIR}/deploy.sh"
+  "${LOGGING_DIR}/deploy.sh"
+  "${MONITORING_DIR}/deploy.sh"
 }
 
 cleanup() {
